b12d3899402874c6a6b8ae16554a550e7733f96b
Yuneido Chacin <146007178+yjchf@users.noreply.github.com>
2024-09-03 02:01:24 +0000

    Pequeñas correcciones de convencion

--- src/app.js
+++ src/app.js
@@ -8,21 +8,28 @@ import "./assets/img/4geeks.ico";
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let combi = [];
-
-function generador() {
+/* Se cambio el nombre de la variable combi a dominios para que sea un nombre más representativo 
+y se colocó dentro de la funcion para evitar conflictos en caso de volver a ejecutar la funcion 
+tambien se cambio su valor de [] a '' para que sea una String vacio */
+function generadorDeDominios() {
+ let dominios = '';
   for (let i = 0; i < pronoun.length; i++) {
-    for (let i2 = 0; i2 < adj.length; i2++) {
-      for (let i3 = 0; i3 < noun.length; i3++) {
-        combi += pronoun[i] + adj[i2] + noun[i3] + ".com<br>";
-        console.log(combi);
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        dominios += pronoun[i] + adj[j] + noun[k] + ".com<br>";
+        console.log(dominios);
       }
     }
   }
-  return combi;
+
+  // Se modifico el nombre de las variables en los bucles para evitar el uso de numeros
+  return dominios;
 }
+//Se modificó el nombre de la funcion a generadorDeDominios
+
+
 window.onload = function() {
   //write your code here
   const list1 = document.getElementById("list");
-  list1.innerHTML = generador();
+  list1.innerHTML = generadorDeDominios();
 };
